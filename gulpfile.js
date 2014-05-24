85dc572d74d9cd8fce63ed01892d19d527c87fa7
Miron Ogrodowicz <miron.ogrodowicz@gmail.com>
2014-05-24 19:35:38 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var connect = require('connect');
+var http = require('http');
+var devApp = connect();
+
+gulp.task('server', function(callback){
+    devApp.use(connect.static('./'));
+
+    var server = http.createServer(devApp).listen(8000, '0.0.0.0');
+    server.on('listening', function(){
+        var devAddress = server.address(),
+            devHost = devAddress === '0.0.0.0' ? 'localhost' : devAddress.address,
+            url = 'http://' + devHost + ':' + devAddress.port;
+
+        console.log('Started webserver at ' + url);
+        callback();
+    });
+})
+
+gulp.task('default', ['server']);
